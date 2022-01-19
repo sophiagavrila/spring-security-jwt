a93d2fe0b57a5c969a6b9f16f3ac54747e9b9430
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-01-18 23:49:38 -0500

    alter SecurityConfig Password Encryption bean - officially allows authentication with previously persisted user obj

--- src/main/java/com/revature/util/JwtUtil.java
+++ src/main/java/com/revature/util/JwtUtil.java
@@ -7,11 +7,13 @@ import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+@Service
 public class JwtUtil {
 
 	/**
